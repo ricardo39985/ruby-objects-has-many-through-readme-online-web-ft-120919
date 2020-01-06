7170d182853bf68ae33b82e8b111de43637d0568
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 19:31:43 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
+  attr_reader :name, :years
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self    
+  end
+  def self.all
+    @@all
+    
+  end
 
 end
