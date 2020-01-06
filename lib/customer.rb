b595bcd4a08991632d87f5b075b8fc8eca33c669
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 20:49:13 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,22 @@
+require "pry"
 class Customer
-  
+  attr_reader :name, :age
+  @@all = []
+  def initialize(name, age)
+    @name= name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+  def meals
+    Meal.all.select { |meal| meal.customer ==  self}
+  end
+  def waiters
+    Meal.all.select{|meal|meal.customer == self}.map { |meal|meal.waiter  }
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
-
+  attr_reader :name, :years
+  @@all = []
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+  end
+  def meals
+    Meal.all.select{|meal|meal.waiter == self}
+  end
+  def best_tipper
+    tip = 0
+    customer = nil
+    Meal.all.each do |meal|
+      if meal.tip > tip
+        tip, customer =meal.tip, meal.customer
+      end
+    end
+    customer
+  end
 end
