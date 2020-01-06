d734c91eb73f7d28444c86ec5f8cbbeeeed13682
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-06 19:35:22 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,14 @@
 class Meal
-
+  attr_reader :waiter, :custmer, :total, :tip
+  @@all = []
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self    
+  end
+  def self.all
+    @@all    
+  end
 end
